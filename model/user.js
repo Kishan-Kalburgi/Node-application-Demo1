d544090e82ba7f5349b6183e60c689a7cfb1d5fb
kishan.ks@hotmail.com <kishan91411>
2018-06-20 00:57:53 -0500

    2nd commit

---
+++ model/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  firstName:  String,
+  lastName: String,
+  gender:   String,
+  dob: { type: Date, default: Date.now },
+  startDate: { type: Date, default: Date.now },
+  endDate: { type: Date, default: Date.now },
+  ethnicity: String
+});
+
+module.exports = mongoose.model('Users', userSchema);
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var User = require("../model/user")
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* save user */
+router.post('/saveUser', function(req, res) {
+  if(req && !req.body){
+    return res.status(403).json({msg: "Please povide proper input"})
+  }
+  var userObj = new User(req.body)
+
+  userObj.save(function(err, data){
+    if(err){
+      res.status(403).json({msg: "Something went wrong  "})
+    } else {
+      res.status(200).json({msg: "User record saved succesfully"})
+    }
+  })
+});
+
+/* Get User List */
+router.get('/getUserList', function(req, res, next) {
+  User.find({}, function(err, results){
+    if(err){
+      res.status(403).json({msg: "Something went wrong  "})
+    } else {
+      res.status(200).json({msg: "User fetched saved succesfully", data: results})
+    }
+  })
+});
+
+/* Update User */
+router.post('/updateUser', function(req, res, next) {
+  
+});
+
+module.exports = router;
